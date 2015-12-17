9ee76d65fde9db60ff06d516bf86b98769ad4f90
Edward Medvedev <edward.medvedev@gmail.com>
2015-12-17 15:28:27 +0600

    Latest release actoin

--- actions/latest_release.py
+++ actions/latest_release.py
@@ -1,1 +1,20 @@
 # parse the changelog for the latest release date
+
+import requests
+import re
+import arrow
+
+from st2actions.runners.pythonrunner import Action
+
+__all__ = [
+    'LatestReleaseAction'
+]
+
+DOCS_URL = 'https://github.com/StackStorm/st2/blob/master/CHANGELOG.rst'
+DATE_MATCH = r'\d+\.\d+\.\d+ - (\w+ \d{2}, \d{4})'
+
+
+class LatestReleaseAction(Action):
+    def run(self):
+        date_string = re.search(DATE_MATCH, requests.get(DOCS_URL).text).groups()[0]
+        return str(arrow.get(date_string, 'MMMM DD, YYYY'))
