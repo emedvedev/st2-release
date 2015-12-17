7112966eb1e705e947718793c17b5b084192da3e
Edward Medvedev <edward.medvedev@gmail.com>
2015-12-18 02:39:35 +0600

    Limited success

--- actions/confirm.py
+++ actions/confirm.py
@@ -7,19 +7,25 @@ __all__ = [
 
 
 class ConfirmAction(Action):
-    def run(self, execution, user, previous):
+    def run(self, user, previous, request_ref, request_timestamp, request_user, request_id):
 
-        if execution.action.ref != 'st2-release.request':
+        if request_ref != 'st2-release.request':
             return {'status': 'wrong ref'}
 
-        if arrow.get(execution.end_timestamp).replace(hours=+2) < arrow.now():
+        # confirmed
+        if arrow.get(request_timestamp).replace(hours=+2) < arrow.now():
             return {'status': 'too old'}
 
-        if execution.result.user == user:
-            return {'status': 'same user'}
+        # confirmed
+        # if request_user == user:
+        #    return {'status': 'same user'}
 
+        #
         for previous_execution in previous:
-            if previous_execution.parameters.id == execution.id:
+            print previous_execution.result
+            print previous_execution.result.confirmation
+            if previous_execution.parameters.id == request_id \
+               and previous_execution.result.confirmation.status == 'success':
                 return {'status': 'duplicate'}
 
-        return {'status': 'success', 'previous': previous}
+        return {'status': 'success'}
