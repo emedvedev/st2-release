603d18f401cbf6b6d7d708a655afefb6c8fe4949
Edward Medvedev <edward.medvedev@gmail.com>
2015-12-17 20:52:24 +0600

    Basic checks

--- actions/confirm.py
+++ actions/confirm.py
@@ -1,7 +1,23 @@
-# Get release date
-# List issues
-# Get the requester
+from st2actions.runners.pythonrunner import Action
 
-# Check if exists and has a correct ref
-# Check for reruns
-# Check if not expired
+__all__ = [
+    'ConfirmAction'
+]
+
+
+class ConfirmAction(Action):
+    def run(self, execution, user):
+
+        # check the execution action ref has the correct type (st2-release.request)
+        # return { 'status': 'wrong ref' }
+
+        # date isn't +2 hours
+        # return { 'status': 'too old' }
+
+        # requester vs confirming user
+        # return { 'status': 'same user' }
+
+        # hasn't been run yet
+        # return { 'status': 'duplicate' }
+
+        return {'status': 'success', 'execution': execution, 'user': user}
